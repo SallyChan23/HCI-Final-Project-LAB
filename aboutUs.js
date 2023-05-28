5c992d4a8a4c6f88e282af4d6af319c97f63ec23
Jeselyn Tania <jeselyntan01@gmail.com>
2023-05-28 20:35:29 +0700

    OK

---
+++ aboutUs.js
@@ -0,0 +1,27 @@
+var clickHereFor = document.getElementById("clickHereFor");
+      if (clickHereFor) {
+        clickHereFor.addEventListener("click", function (e) {
+          window.location.href = "contactUs.html";
+        });
+      }
+      
+      var aboutUsText1 = document.getElementById("aboutUsText1");
+      if (aboutUsText1) {
+        aboutUsText1.addEventListener("click", function (e) {
+          window.location.href = "aboutUs.html";
+        });
+      }
+      
+      var contactUsText = document.getElementById("contactUsText");
+      if (contactUsText) {
+        contactUsText.addEventListener("click", function (e) {
+          window.location.href = "contactUs.html";
+        });
+      }
+      
+      var locationsText = document.getElementById("locationsText");
+      if (locationsText) {
+        locationsText.addEventListener("click", function (e) {
+          window.location.href = "location.html";
+        });
+      }
---
+++ home.js
@@ -0,0 +1,55 @@
+var aboutUsText = document.getElementById("aboutUsText");
+if (aboutUsText) {
+  aboutUsText.addEventListener("click", function (e) {
+    window.location.href = "aboutUs.html";
+  });
+}
+
+var locationsText = document.getElementById("locationsText");
+if (locationsText) {
+  locationsText.addEventListener("click", function (e) {
+    window.location.href = "location.html";
+  });
+}
+
+var contactUsText = document.getElementById("contactUsText");
+if (contactUsText) {
+  contactUsText.addEventListener("click", function (e) {
+    window.location.href = "contactUs.html";
+  });
+}
+
+var productsText = document.getElementById("productsText");
+if (productsText) {
+  productsText.addEventListener("click", function (e) {
+    window.location.href = "products.html";
+  });
+}
+
+var productsText1 = document.getElementById("productsText1");
+if (productsText1) {
+  productsText1.addEventListener("click", function (e) {
+    window.location.href = "products.html";
+  });
+}
+
+var aboutUsText1 = document.getElementById("aboutUsText1");
+if (aboutUsText1) {
+  aboutUsText1.addEventListener("click", function (e) {
+    window.location.href = "aboutUs.html";
+  });
+}
+
+var contactUsText1 = document.getElementById("contactUsText1");
+if (contactUsText1) {
+  contactUsText1.addEventListener("click", function (e) {
+    window.location.href = "contactUs.html";
+  });
+}
+
+var locationsText1 = document.getElementById("locationsText1");
+if (locationsText1) {
+  locationsText1.addEventListener("click", function (e) {
+    window.location.href = "location.html";
+  });
+}
