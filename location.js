cf7cf7a49bc1552d7937053d9a2b3eda0d558146
Jeselyn Tania <jeselyntan01@gmail.com>
2023-05-31 14:35:22 +0700

    OK

---
+++ location.js
@@ -0,0 +1,27 @@
+var orClickHere = document.getElementById("orClickHere");
+      if (orClickHere) {
+        orClickHere.addEventListener("click", function (e) {
+          window.location.href = "contactUs.html";
+        });
+      }
+      
+      var aboutUsText = document.getElementById("aboutUsText");
+      if (aboutUsText) {
+        aboutUsText.addEventListener("click", function (e) {
+          window.location.href = "aboutUs.html";
+        });
+      }
+      
+      var contactUsText = document.getElementById("contactUsText");
+      if (contactUsText) {
+        contactUsText.addEventListener("click", function (e) {
+          window.location.href = "contactUs.html";
+        });
+      }
+      
+      var locationsText = document.getElementById("locationsText");
+      if (locationsText) {
+        locationsText.addEventListener("click", function (e) {
+          window.location.href = "location.html";
+        });
+      }
