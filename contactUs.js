174871c8a64efdf28fe38a461cb0a0033ac2ca3e
Jeselyn Tania <jeselyntan01@gmail.com>
2023-06-01 13:08:42 +0700

    OK

--- contactUs.js
+++ contactUs.js
@@ -1,38 +1,37 @@
-function getInt(a,b,c){
-
-}
-
-
 document.getElementById('form').addEventListener('submit', (event)=>{
     event.preventDefault()
-    const useremail= document.getElementById('usermail').value
-    const username=document.getElementById('username').value
-    const agreement= document.getElementById('agree').checked
-
+    const useremail = document.getElementById('usermail').value
+    const username = document.getElementById('username').value
+    const agreement = document.getElementById('agree').checked
+    const message = document.getElementById('message').value
+    const country = document.getElementById('country')
     console.log(username, useremail, agreement);
 
-    if(username.length<8 || username.length>15){
-        alert('Username length must be between 8-15 characters')
+    if(username.length<5 || username.length>15){
+        alert('Name length must be between 8-15 characters')
         return
     }
 
-    if(!useremail.endsWith('@gmail.com') || !useremail.endsWith('@yahoo.com') || !useremail.endsWith('@ac.id') || !useremail.endsWith('@outlook.com')){
-        alert('Email must contains right format')
-        return
-    }
-    if(password!==repassword){
-        alert('Retype password doesn\'t match')
+    if(!useremail.endsWith('@gmail.com')){
+        alert('Email must contains with @gmail.com at the end')
         return
     }
 
+    if(message.length<10){
+        alert('Message length must be 10 or more')
+        return;
+    }
+
+    if(country.value===""){
+        alert('You must select your country')
+        return;
+    }
     if(!agreement){
         alert('You must agree with all of term and conditions!')
         return
     }
 
-    alert('Registration Success')
-    window.location.href='/home.html'
+    alert('Send Success')
+    window.location.href='home.html'
 })
-
-// document.getElementById('register-button').addEventListener('click')
 
