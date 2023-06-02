815acbe7c56bde8b123b423015c7f8e83b99b048
Jeselyn Tania <jeselyntan01@gmail.com>
2023-06-02 22:37:23 +0700

    OK

---
+++ products.js
@@ -0,0 +1,41 @@
+var rolexLogoDesign22Image = document.getElementById("rolexLogoDesign22Image");
+      if (rolexLogoDesign22Image) {
+        rolexLogoDesign22Image.addEventListener("click", function (e) {
+          window.location.href = "Rolex.html";
+        });
+      }
+      
+      var seiokoLogo2Image = document.getElementById("seiokoLogo2Image");
+      if (seiokoLogo2Image) {
+        seiokoLogo2Image.addEventListener("click", function (e) {
+          window.location.href = "seiko.html";
+        });
+      }
+      
+      var gShockLogo2Image = document.getElementById("gShockLogo2Image");
+      if (gShockLogo2Image) {
+        gShockLogo2Image.addEventListener("click", function (e) {
+          window.location.href = "Gshock.html";
+        });
+      }
+      
+      var aboutUsText = document.getElementById("aboutUsText");
+      if (aboutUsText) {
+        aboutUsText.addEventListener("click", function (e) {
+          window.location.href = "aboutUs.html";
+        });
+      }
+      
+      var contactUsText = document.getElementById("contactUsText");
+      if (contactUsText) {
+        contactUsText.addEventListener("click", function (e) {
+          window.location.href = "contactUs.html";
+        });
+      }
+      
+      var locationsText = document.getElementById("locationsText");
+      if (locationsText) {
+        locationsText.addEventListener("click", function (e) {
+          window.location.href = "location.html";
+        });
+      }
